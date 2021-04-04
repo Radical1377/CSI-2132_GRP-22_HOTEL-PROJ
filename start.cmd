3078d8847eb85f437c43d191d66ac531dba40bdc
Radical1377 <armankompany@hotmail.com>
2021-04-03 20:03:56 -0400

    Fixed batch script

--- start.cmd
+++ start.cmd
@@ -28,20 +28,17 @@ ECHO Group 22 Submission, CSI 2132
 ECHO Database Designed and Populated by: Adam Nykorak, Bryson Crooks
 ECHO Java Client Backend, Frontend and Launcher Developed by: Arman Kompany Zare
 ECHO ============================================================================
-ECHO.
 
-WHERE JAVA > NUL 2> NUL
-IF ERRORLEVEL 1 (
-	IF NOT EXIST jdk-16\ (
-    	ECHO [!] Java not detected on PATH!
-		ECHO [+] Downloading and Installing JDK . . .
-		CURL -# -o jdk.zip -L -b oraclelicense=accept-securebackup-cookie https://download.oracle.com/otn-pub/java/jdk/16+36/7863447f0ab643c585b9bdebf67c69db/jdk-16_windows-x64_bin.zip
-    	ECHO [+] Extracting JDK . . .
-		TAR -xf jdk.zip
-		DEL jdk.zip
-	)
-	SET PATH=%CD%\jdk-16\bin;%PATH%
+IF NOT EXIST jdk-16\ (
+   	ECHO [!] Java not detected on PATH!
+	ECHO [+] Downloading and Installing JDK . . .
+	CURL -# -o jdk.zip -L -b oraclelicense=accept-securebackup-cookie https://download.oracle.com/otn-pub/java/jdk/16+36/7863447f0ab643c585b9bdebf67c69db/jdk-16_windows-x64_bin.zip
+   	ECHO [+] Extracting JDK . . .
+	TAR -xf jdk.zip
+	DEL jdk.zip
 )
+SET PATH=%CD%\jdk-16\bin;%PATH%
+
 ECHO [+] Setting up CLASSPATH . . .
 SET CLASSPATH=%cd%\postgresql-42.2.19.jar;.
 ECHO [+] Compiling .java files . . .
